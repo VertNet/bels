2a856a5b4da48bc2828c7440ce0445badfa5e497
Marie-Elise Lecoq <melecoq@vertnet.org>
2021-01-21 15:39:26 -0800

    job: add gcp cloud function entrypoint

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+This file is provided as a wrapper to execute code from google cloud functions
+"""
+
+import bels.job
+
+
+def csv_processing_entrypoint(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+
+    bels.job.process_csv(event, context)
